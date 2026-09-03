63250ca452dc10b8c1db665fe1a28cd2f1112108
1229302Fenyvesi <fenyvesiwi@gmail.com>
2026-09-03 09:47:27 +0200

    relocatet the test propperties file and fixed the connection to it, so no dynamic implicit extra test envirenmont obverrides my setup

--- backend/src/test/java/ibee/webapp/todo_app/BaseIntegrationTest.java
+++ backend/src/test/java/ibee/webapp/todo_app/BaseIntegrationTest.java
@@ -4,10 +4,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(/*webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT*/)
 public abstract class BaseIntegrationTest {
 
-    @DynamicPropertySource
+  /*   @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", () -> "jdbc:mariadb://localhost:3308/todo_test");
         registry.add("spring.datasource.username", () -> "root");
@@ -23,4 +23,5 @@ public abstract class BaseIntegrationTest {
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
         registry.add("jwt.secret.key", () -> "4de22fc54d5239beb2fc60cf9081ba8838cddea3ec66dcc3e4a242035b1850a31f34dd61d8e26f8f6730eafa198f39651bf50ee89a556b39374be5507ed5036f");
     }
+*/
 }
--- backend/src/test/java/ibee/webapp/todo_app/TodoAppApplicationTests.java
+++ backend/src/test/java/ibee/webapp/todo_app/TodoAppApplicationTests.java
@@ -2,8 +2,10 @@ package ibee.webapp.todo_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TodoAppApplicationTests extends BaseIntegrationTest {
 
 	@Test
